b5b31c51b65c13f9d95d80fdd46221952cb359cc
TheMagnificentMiles <milessayshi7@gmail.com>
2022-12-24 15:34:26 -0500

    Thing

--- DynaEngine/AppWindow.h
+++ DynaEngine/AppWindow.h
@@ -4,6 +4,7 @@
 #include "SwapChain.h"
 #include "DeviceContext.h"
 #include "VertexBuffer.h"
+#include "ConstantBuffer.h"
 #include "VertexShader.h"
 #include "PixelShader.h"
 
@@ -18,9 +19,14 @@ public:
 	virtual void onDestroy() override;
 
 private:
+	unsigned long m_old_time = 0;
+	float m_delta_time = 0;
+	float m_angle = 0;
+
 	SwapChain* m_swap_chain;
 	VertexBuffer* m_vb;
 	VertexShader* m_vs;
 	PixelShader* m_ps;
+	ConstantBuffer* m_cb;
 };
 
